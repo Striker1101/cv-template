39fe76cf43f786ea9a440a35edb8367ca6d0327f
striker <goodluckodemilin@gmail.com>
2022-12-05 19:06:02 +0100

    startup

---
+++ src/CV-template.js
@@ -0,0 +1,13 @@
+import React from 'react'
+export default class Template extends React.Component {
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+            <>
+
+            </>
+        )
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Template from "./CV-template";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Template />
+  </React.StrictMode>
+);
