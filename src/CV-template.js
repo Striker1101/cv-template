334a937f45ab47cbb06b23d619e67e3e64ce4b41
striker <goodluckodemilin@gmail.com>
2023-01-10 04:51:09 +0100

    setup deploy

--- src/CV-template.js
+++ src/CV-template.js
@@ -14,27 +14,43 @@ export default class Template extends React.Component {
     this.state = {
       education: "Education",
       experience: "Experience",
-      modal: false,
+      change: false,
+      modal: false
     };
     this.generate = this.generate.bind(this);
     this.clear = this.clear.bind(this);
     this.toggle = this.toggle.bind(this);
   }
-  clear() {}
+
+  clear() {
+    this.setState({
+      change: !this.state.change,
+      education: "Education",
+      experience: "Experience",
+      modal: this.state.modal
+    });
+    console.log(this.state.change);
+  }
   toggle() {
     this.setState({
       education: "Education",
       experience: "Experience",
       modal: !this.state.modal,
+      change: this.state.change
     });
   }
   generate = () => {
+    let forms = [...document.querySelectorAll(".form")];
+    forms.forEach((form) => {
+      form.style.cssText = "display:none;";
+    });
     document.querySelector(".generate").style.cssText = "display: none";
+    document.querySelector(".clearCV").style.cssText = "display: none";
     const input = document.getElementById("container");
     html2canvas(input, {
       logging: true,
       letterRendering: 0,
-      useCORS: true,
+      useCORS: true
     }).then((canvas) => {
       const imgWidth = 208;
       const imgHeight = (canvas.height * imgWidth) / canvas.width;
@@ -43,30 +59,37 @@ export default class Template extends React.Component {
       pdf.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
       pdf.save("generated.pdf");
     });
+    forms.forEach((form) => {
+      form.style.cssText = "display:flex;";
+    });
+    document.querySelector(".generate").style.cssText = "display: block";
+    document.querySelector(".clearCV").style.cssText = "display: block";
   };
   render() {
     return (
       <div id="container">
-        <Profile />
+        <Profile clear={this.state.change} />
         <div className="content">
           <div className="first">
-            <Education value={this.state.education} />
+            <Education clear={this.state.clear} value={this.state.education} />
             <Education value={this.state.experience} />
           </div>
           <div className="second">
             <div>
-              <Contact />
-              <Skills />
+              <Contact clear={this.state.clear} />
+              <Skills clear={this.state.clear} />
             </div>
             <div>
-              <Accounts />
+              <Accounts clear={this.state.clear} />
             </div>
             <div>
               {" "}
               <button onClick={this.generate} className="generate">
                 Generate pdf
               </button>
-              <button onClick={this.toggle}>Clear CV</button>
+              <button onClick={this.toggle} className="clearCV">
+                Clear CV
+              </button>
               {this.state.modal && (
                 <div className="modal">
                   <div className="overlay" onClick={this.toggle}>
@@ -78,7 +101,9 @@ export default class Template extends React.Component {
                       <button className="close-modal" onClick={this.toggle}>
                         close
                       </button>
-                      <button onClick={this.clear}>Clear</button>
+                      <button onClick={this.clear} className="clear">
+                        Clear
+                      </button>
                     </div>
                   </div>
                 </div>
